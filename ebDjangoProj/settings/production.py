8a76ccf49ccc0b8db86d4f669a44e05fa3d6d85c
mconceicaonlm <mconceicaonlm@outlook.com>
2017-11-14 22:54:54 +0000

    add production settings3

--- ebDjangoProj/settings/production.py
+++ ebDjangoProj/settings/production.py
@@ -76,17 +76,13 @@ AWS_SECRET_ACCESS_KEY = 'tmTe5qxk9CRpO/oK/hnxh6NH+trIdbPy8QK82VOk'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-if 'RDS_HOSTNAME' in os.environ:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': os.environ['RDS_DB_NAME'],
-            'USER': os.environ['RDS_USERNAME'],
-            'PASSWORD': os.environ['RDS_PASSWORD'],
-            'HOST': os.environ['RDS_HOSTNAME'],
-            'PORT': os.environ['RDS_PORT'],
-        }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
