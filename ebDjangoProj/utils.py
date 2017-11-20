56f7178c429ee0b235e6572e0abaf2e10d7845af
martinm <mconceicaonlm@outlook.com>
2017-11-20 12:38:07 +0000

    eb extension modif 2

--- ebDjangoProj/utils.py
+++ ebDjangoProj/utils.py
@@ -1,6 +1,6 @@
-from storages.backends.s3boto import S3BotoStorage
+from storages.backends.s3boto3 import S3Boto3Storage
 
-StaticRootS3BotoStorage =lambda: S3BotoStorage(location='static') # bucket name / static
-MediaRootS3BotoStorages=lambda: S3BotoStorage(location='media') # bucket name / media
+StaticRootS3BotoStorage =lambda: S3Boto3Storage(location='static') # bucket name / static
+MediaRootS3BotoStorages=lambda: S3Boto3Storage(location='media') # bucket name / media
 
 #ProtectedRootS3BotoStorage = lambda: S3Boto3Storage (location = 'protected') # bucket name / protected
